d4bd469987310412399686dd2354f0e86a5d0884
ronald <ottoniel.21@outlook.es>
2020-10-22 23:22:32 -0600

    a

--- clinica/src/app/app.component.ts
+++ clinica/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from './Service/data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ClinicaWeb';
+  title = 'MultiClinicaWeb';
+
+  constructor(public json:DataService, private router:Router){
+    this.json.getEmployee('/ClinicaWeb-1.0-SNAPSHOT/crudemployee').subscribe((res: any) => {
+      console.log(res);
+    })
+  }
+  
+  List(){
+    this.router.navigate(["list"]);
+  }
 }
--- clinica/src/app/app.module.ts
+++ clinica/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListComponent } from './Employee/list/list.component';
+import {FormsModule} from '@angular/forms';
+import {DataService} from '../app/Service/data.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
